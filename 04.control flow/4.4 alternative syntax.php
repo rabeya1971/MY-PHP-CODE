ec78dee0b7786e00a69b7b688ec7b4d7ab2398c5
rabeya1971 <sultanarabeya21@gmail.com>
2025-07-12 12:12:23 +0600

    class 8 added

---
+++ 04.control flow/4.4 alternative syntax.php
@@ -0,0 +1,54 @@
+<?php
+if($condition){
+    //statements
+}else{
+    //statements
+}
+
+//Alternative syntax
+
+if($condition):
+    //statements
+ else:
+    //statements
+ endif; 
+
+
+
+ if($condition){
+    //statements
+}elseif($anotherCondition){
+    //statements
+}else{
+    //statements
+}
+
+//Alternative syntax
+
+if($condition):
+    //statements
+elseif($anotherCondition):
+    //statements
+ else:
+    //statements
+ endif; 
+
+ while ($condition){
+    //statements
+ }
+
+ //Alternative syntax
+ while($condition):
+    //statements
+ endwhile;
+
+ for( ; ; ){
+    //statements
+ }
+
+ //Alternative syntax:php code html er moddhe jkhn likhi
+ for( ; ; ):
+    //statements
+ endfor;
+
+ 
--- index.php
+++ index.php
@@ -1,83 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+   <style>
+      table{
+         border-collapse: collapse;
+      }
+      td{
+         border: 1px solid #ddd;
+         padding: 12px;
+      }
+   </style>
 </head>
-<body style="font-size:40 px;margin-left:80px;">
-    <?php
-    echo "Hi, Future Developers! <br>";
-    //variable
-    $name = "Rabeya Sultana<br>";
-    echo$name;
-    //Best Practice
-    // $my_name(snakeCase-variable/function)
-    // $myName(camelCase-method)
-    // $MyName(pascalCase-class)
-    $name = "Rabeya<br>";
-    echo$name;
-    //constant
-    define("PI", 3.1416);
-    echo"PI<br>";
+<body style="margin-left:10px;font-size:40px">
 
-    /*Data Types
-
-       Scalar Types
-          Integer
-          Float/Double
-          String
-          Boolean
-       Compound Types
-          Array
-          Object
-       spacial Types
-          Null
-          Resource
-
-    */
-    $age = 25;
-    $price = 40.43;
-    $myName = "Rabeya Sultana";
-    $is_admin = false;
-
-    $fruits = ["Mango", "Banana", 27];
-    echo "$fruits[0]<br>";
-    // $user1 = new User();
-
-    // $variable = Null;
-    // $file = fopen("test.txt", "r");
-
-    //Arithmetic Operators
-    $a = 15;
-    $b = 4;
-    $addition = $a + $b;
-    $subtraction = $a - $b;
-    $multiplition = $a * $b;
-    $division = $a / $b;
-    $modulus = $a % $b;
-    $exponentiation= $a ** $b;
-
-    echo "$addition<br>";
-    echo "$subtraction<br>";
-    echo "$multiplition<br>";
-    echo "$division <br>";
-    echo "$modulus<br>";
-    echo "$exponentiation<br>";
-    //Assignment Operators
-
-    $a += 2;    //$a = $a + 2;
-    echo "$a<br>";
-    $a -= 2;    //$a = $a - 2;
-    echo "$a<br>";
-    $a *= 2;    //$a = $a * 2;
-    echo "$a<br>";
-    $a /= 2;    //$a = $a / 2;
-    echo "$a<br>";
-    $a %= 2;    //$a = $a % 2;
-    echo "$a<br>";
+<!-- <table>
+   <tr>
+      <td>1</td>
+      <td>2</td>
+      <td>3</td>
+   </tr>
+   <tr>
+      <td>4</td>
+      <td>5</td>
+      <td>6</td>
+   </tr>
+   <tr>
+      <td>7</td>
+      <td>8</td>
+      <td>9</td>
+   </tr>
+</table> -->
+<!-- 
+<table>
+   <?php for( $i = 1 ; $i <=5 ; $i++ ): ?>
+      <tr>
+         <?php for( $j = 1; $j <= 5 ; $j++ ): ?>
+         <td> <?php echo $i * $j ?> </td>
+         <?php endfor; ?>
+      </tr>
+   <?php endfor; ?>
    
-?>
+</table> -->
 
+<!-- 
+<table>
+   <?php for( $i = 1 ; $i <=10 ; $i++ ): ?>
+      <tr>
+         <?php for( $j = 1; $j <= 10 ; $j++ ): ?>
+         <td> <?php echo $i * $j ?> </td>
+         <?php endfor; ?>
+      </tr>
+   <?php endfor; ?>
+   
+</table> -->
+
+
+<table>
+   <?php for( $i = 1 ; $i <=15 ; $i++ ): ?>
+      <tr>
+         <?php for( $j = 1; $j <= 15 ; $j++ ): ?>
+         <td> <?php echo $i * $j ?> </td>
+         <?php endfor; ?>
+      </tr>
+   <?php endfor; ?>
+   
+</table>
+
+<!-- <ul>
+   <li>Apple</li>
+   <li>Banana</li>
+   <li>Orange</li>
+</ul> -->
+   
+<?php $fruits = ["Apple", "Banana", "Orange", "Jackfruit" ];?>
+<ul>
+   <?php foreach ( $fruits as $fruit ): ?>
+   <li> <?php echo $fruit; ?> </li>
+   <?php endforeach; ?>
+   
+</ul>
 </body>
 </html>
