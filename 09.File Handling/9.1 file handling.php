b229ea66aa7a31bcb980b800f5777b992892bb8d
rabeya1971 <sultanarabeya21@gmail.com>
2025-07-15 21:10:19 +0600

    Class 10 added

---
+++ 09.File Handling/9.1 file handling.php
@@ -0,0 +1,70 @@
+<?php
+// $file = fopen("new.txt", "w");
+// fwrite($file, "Projukti Plus");
+
+// $file = fopen("new.txt", "r");
+// echo fread($file, filesize("new.txt"));
+// fclose($file);
+
+// // rename("new.txt", "anotherFile.txt");
+// mkdir("ProjuktiPlus");
+// // rename("anotherFile.txt", "new.txt",);
+
+// file_put_contents("test.txt", "Projukti Plus-");
+// file_put_contents("test.txt", "Jekhane projuktir golpo hoi bangla vashay\n", FILE_APPEND);
+// echo file_get_contents("test.txt");
+
+// file_put_contents("projuktiPlus/newFile.txt", "notun file");
+// echo file_get_contents("projuktiPlus/newFile.txt");
+
+//nirdisto folder a notun file create kora:
+// file_put_contents("09.File Handling/newwFile.txt", "notun ffile");
+// echo file_get_contents("09.File Handling/newwFile.txt");
+
+
+//with variable
+
+// $fileName = "test.txt";
+// $fileContent = "Projukti Plus-";
+
+// file_put_contents($fileName, $fileContent);
+// $fileContent2 = "Jekhane projuktir golpo hoi bangla vashay\n";
+// file_put_contents($fileName, $fileContent2, FILE_APPEND);
+// echo file_get_contents($fileName);
+
+// file_put_contents("projuktiPlus/newFile.txt", "notun file\n");
+// echo file_get_contents("projuktiPlus/newFile.txt");
+
+
+
+// $myFolder = "projuktiPlus";
+// if(!is_dir($myFolder)){
+//     mkdir($myFolder);
+// }else{
+//     echo "$myFolder folder already exist.";
+// }
+
+// $myFolder = "projukti";
+// if(!is_dir($myFolder)){
+//     mkdir($myFolder);
+// }else{
+//     echo "$myFolder folder already exist.";
+// }
+
+// file_exists();
+// is_readable();
+// is_writable();
+
+
+
+// echo __DIR__;//directory check
+
+// $fileName = "C:/xampp/htdocs/MY-PHP-CODE/09.File Handling/test.txt
+// ";
+// if (file_exists($fileName)){
+//     echo file_get_contents($fileName);
+//}//else{
+//     echo "file ti pawa jaini";
+// }
+
+
